f2293252c84a1e5290a1a43174d456c8f09845af
Vitor Paladini <109686724+paladini-qa@users.noreply.github.com>
2025-05-06 19:20:29 -0300

    feat: implemento da criação de usuario (#3)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AgendaCompromissoPOO.Modelos;
+
+Console.WriteLine("Digite o nome do Usuario:");
+string nome = Console.ReadLine()!;
+while (string.IsNullOrEmpty(nome))
+{
+  Console.WriteLine("Nome inválido, digite novamente:");
+  nome = Console.ReadLine()!;
+}
+try
+{
+  Usuario usuario = new()
+  {
+    NomeCompleto = nome
+  };
+  Console.WriteLine($"Usuario {usuario.NomeCompleto} criado com sucesso!");
+}
+catch (Exception ex)
+{
+  Console.WriteLine($"Erro: {ex.Message}");
+}
