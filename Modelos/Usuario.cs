4157e206954627bf7d2f32b7ffe28f17260eca4a
paladini-qa <paladini.qa@gmail.com>
2025-05-06 19:05:44 -0300

    feat: implementar classe usuario

--- Modelos/Usuario.cs
+++ Modelos/Usuario.cs
@@ -1,8 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AgendaCompromissoPOO.Modelos;
 
 public class Usuario
 {
+  public string NomeCompleto { get; set; }
 
+  private readonly List<Compromisso> _compromissos = new();
+
+  public IReadOnlyCollection<Compromisso> Compromissos => _compromissos.AsReadOnly();
+
+  public void AdicionarCompromisso(Compromisso compromisso)
+  {
+    if (compromisso == null)
+    {
+      throw new ArgumentNullException(nameof(compromisso), "O compromisso não pode ser nulo.");
+    }
+
+    if (_compromissos.Any(c => c.Data == compromisso.Data))
+    {
+      throw new InvalidOperationException("Já existe um compromisso agendado para esta data e hora.");
+    }
+
+    _compromissos.Add(compromisso);
+  }
 }
