7405093bd64fc8f576c9c690f64598af916a4e25
Vitor Paladini <109686724+paladini-qa@users.noreply.github.com>
2025-05-07 13:05:21 -0300

    B pablo (#8)
    * Implementado Anotacao
    
    * Implementado Compromisso
    
    Tá faltando algumas coisas ainda.
    
    ---------
    
    Co-authored-by: PabloEC382 <pablolima@alunos.utfpr.edu.br>

--- Modelos/Anotacao.cs
+++ Modelos/Anotacao.cs
@@ -4,5 +4,13 @@ namespace AgendaCompromissoPOO.Modelos;
 
 public class Anotacao
 {
+    public string TextoAnotacao { get; set; } = string.Empty;
+    public dateTime DataCriacao { get; } = DateTime.Now;
+
+    public Anotacao(string textoAnotacao)
+    {
+        TextoAnotacao = textoAnotacao;
+        DataCriacao = DateTime.Now;
+    }
 
 }
--- Modelos/Compromisso.cs
+++ Modelos/Compromisso.cs
@@ -5,4 +5,47 @@ namespace AgendaCompromissoPOO.Modelos;
 public class Compromisso
 {
   public DateTime Data { get; set; }
+  public TimeSpan Hora { get; set; }
+  public string Descricao { get; set; } = string.Empty;
+
+  private readonly List<Anotacao> _anotacoes = new();
+  public IReadOnlyCollection<Anotacao> Anotacoes => _anotacoes.AsReadOnly();
+
+  public Compromisso(DateTime data, TimeSpan hora, string descricao){
+    var dataHora = data.Date + hora;
+
+    if (dataHora <=DateTime.Now)
+    {
+      throw new ArgumentException("A data do compromisso deve ser no futuro.");
+    }
+    
+    if(string.IsNullOrWhiteSpace(descricao)){
+      throw new ArgumentException("A descrição do compromisso não pode ser nula.");
+    }
+
+    Data = data.Date;
+    Hora = hora;
+    Descricao = descricao;
+    // tem que adicionar o usuario
+  }
+
+  public void AdicionarAnotacao(string textoAnotacao)
+  {
+    if (string.IsNullOrWhiteSpace(textoAnotacao))
+    {
+      throw new ArgumentException("O texto da anotação não pode ser nulo.");
+    }
+
+    _anotacoes.Add(new Anotacao(textoAnotacao));
+  }
+
+  //Falta adicionar participante
+
+  public override string ToString(){
+    return  $"Compromisso data{Data:dd/MM/yyyy} hora {Hora:hh:mm} - {Descricao}" +
+            $"\nDescrição: {Descricao}" +
+            $"\nAnotações: {_anotacoes.count}";
+            //Falta implementar a ToSring do local, participantes e usuario
+  }
+
 }
