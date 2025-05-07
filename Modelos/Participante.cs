c0a4bded6caf20a22b7fc1e263790753dcd6ae1a
jeyjey25 <jeanmatiaskuhn@alunos.utfpr.edu.br>
2025-05-07 09:12:33 -0300

    feito classe Participante (#6)
    estrutura da classe Participante

--- Modelos/Participante.cs
+++ Modelos/Participante.cs
@@ -1,8 +1,35 @@
 using System;
+using System.Collections.Generic;
 
-namespace AgendaCompromissoPOO.Modelos;
-
-public class Participante
+namespace AgendaCompromissoPOO.Modelos 
 {
+    public class Participante
+    {
+        public string Nome { get; private set; }
+        private List<Compromisso> compromissos;
+        public IReadOnlyCollection<Compromisso> Compromissos => compromissos.AsReadOnly();
+        public Participante(string nome)
+        {
+            if (string.IsNullOrWhiteSpace(nome))
+                throw new ArgumentException("o nome do participante e obrigatorio");
 
+            Nome = nome;
+            compromissos = new List<Compromisso>();
+        }
+        public void AdicionarCompromisso(Compromisso compromisso)
+        {
+            if (compromisso == null)
+                throw new ArgumentNullException(nameof(compromisso));
+
+            if (!compromissos.Contains(compromisso))
+            {
+                compromissos.Add(compromisso);
+                compromisso.AdicionarParticipante(this); 
+            }
+        }
+                public override string ToString()
+        {
+            return Nome;
+        }
+    }
 }
