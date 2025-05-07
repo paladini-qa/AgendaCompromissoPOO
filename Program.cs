bf86bdd2a4f58e74e9a5164872873d1444986efa
paladini-qa <paladini.qa@gmail.com>
2025-05-07 09:31:54 -0300

    feat: implementar a validação de participantes por local

--- Program.cs
+++ Program.cs
@@ -56,7 +56,76 @@ catch (Exception ex)
   Console.WriteLine($"\nErro: {ex.Message}");
 }
 
-// FALTA CADASTRAR O COMPROMISSO
+Console.WriteLine("\nVamos cadastrar um compromisso!");
+
+// FALTA CADASTRAR DATA, HORA, DESCRICAO E ANOTAÇÕES DO COMPROMISSO
+
+Console.WriteLine("\nDigite o nome do local do compromisso:");
+string localCompromisso = Console.ReadLine()!;
+while (string.IsNullOrEmpty(localCompromisso))
+{
+  Console.WriteLine("\nLocal inválido, digite novamente:");
+  localCompromisso = Console.ReadLine()!;
+}
+while (localCompromisso != nomeLocal)
+{
+  Console.WriteLine("\nErro: O local do compromisso não corresponde ao local cadastrado.");
+  Console.WriteLine("\nDigite o nome do local do compromisso:");
+  localCompromisso = Console.ReadLine()!;
+}
+try
+{
+  Console.WriteLine($"\nLocal do compromisso confirmado: {localCompromisso}");
+}
+catch (Exception ex)
+{
+  Console.WriteLine($"\nErro: {ex.Message}");
+}
+
+Console.WriteLine("\nQuer adicionar participantes ao compromisso? (s/n)");
+string resposta = Console.ReadLine()!;
+int quantidadeParticipantes = 1;
+if (quantidadeParticipantes >= capacidadeMaxima)
+{
+  Console.WriteLine("\nErro: A quantidade de participantes excede a capacidade máxima do local.");
+  Console.WriteLine("\nNenhum participante adicionado ao compromisso.");
+}
+else
+{
+  while (resposta.ToLower() != "s" && resposta.ToLower() != "n")
+  {
+    Console.WriteLine("\nResposta inválida, digite 's' para sim ou 'n' para não:");
+    resposta = Console.ReadLine()!;
+  }
+  if (resposta.ToLower() == "s")
+  {
+    Console.WriteLine("\nDigite o nome do participante:");
+    string nomeParticipante = Console.ReadLine()!;
+    while (string.IsNullOrEmpty(nomeParticipante))
+    {
+      Console.WriteLine("\nNome do participante inválido, digite novamente:");
+      nomeParticipante = Console.ReadLine()!;
+    }
+    try
+    {
+      Participante participante = new(nomeParticipante);
+      quantidadeParticipantes++;
+      //participante.AdicionarCompromisso(compromisso);
+      Console.WriteLine($"\nParticipante {participante.Nome} adicionado com sucesso!");
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"\nErro: {ex.Message}");
+    }
+    Console.WriteLine($"\nParticipante {nomeParticipante} adicionado ao compromisso. Total de participantes: {quantidadeParticipantes}.");
+  }
+  else
+  {
+    Console.WriteLine("\nNenhum participante adicionado ao compromisso.");
+  }
+}
+
+
 
 // FALTA VALIDAR A QUANTIDADE DE PARTICIPANTES NO LOCAL
 
