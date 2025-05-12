a3d27bd512c0125a024875cade2037b295d45256
PabloEC382 <pablolima@alunos.utfpr.edu.br>
2025-05-12 09:15:38 -0300

    atualização Compromisso.cs main
    Atualizei o código da main com o que estava na minha branch. #10

--- Modelos/Compromisso.cs
+++ Modelos/Compromisso.cs
@@ -8,30 +8,40 @@ public class Compromisso
   public TimeSpan Hora { get; set; }
   public string Descricao { get; set; } = string.Empty;
 
+  public Usuario usuario { get; set; } = new Usuario();
+  public Local local { get; set; } = new Local();
+
+  private readonly List<Participante> _participantes = new();
+  public IReadOnlyCollection<Participante> Participantes => _participantes.AsReadOnly();
+
   private readonly List<Anotacao> _anotacoes = new();
   public IReadOnlyCollection<Anotacao> Anotacoes => _anotacoes.AsReadOnly();
 
-  public Compromisso(DateTime data, TimeSpan hora, string descricao)
-  {
+  public Compromisso(DateTime data, TimeSpan hora, string descricao){
     var dataHora = data.Date + hora;
 
-    if (dataHora <= DateTime.Now)
+    if (dataHora <=DateTime.Now)
     {
       throw new ArgumentException("A data do compromisso deve ser no futuro.");
     }
-
-    if (string.IsNullOrWhiteSpace(descricao))
-    {
+    
+    if(string.IsNullOrWhiteSpace(descricao)){
       throw new ArgumentException("A descrição do compromisso não pode ser nula.");
     }
 
     Data = data.Date;
     Hora = hora;
     Descricao = descricao;
-    // tem que adicionar o usuario
+
+    usuario = usuario ?? throw new ArgumentNullException(nameof(usuario), "O usuário não pode ser nulo.");
+    local = local ?? throw new ArgumentNullException(nameof(local), "O local não pode ser nulo.");
   }
 
-  public void AdicionarAnotacao(string textoAnotacao)
+    public Compromisso()
+    {
+    }
+
+    public void AdicionarAnotacao(string textoAnotacao)
   {
     if (string.IsNullOrWhiteSpace(textoAnotacao))
     {
@@ -41,14 +51,22 @@ public class Compromisso
     _anotacoes.Add(new Anotacao(textoAnotacao));
   }
 
-  //Falta adicionar participante
+  public void AdicionarParticipante(Participante participante)
+  {
+    if (_participantes.Contains(participante))
+    {
+      return;
+    }
+    _participantes.Add(participante);
+  }
 
   public override string ToString()
   {
     return $"Compromisso data{Data:dd/MM/yyyy} hora {Hora:hh:mm} - {Descricao}" +
-            $"\nDescrição: {Descricao}" +
+            $"\nusuário: {usuario.NomeCompleto}" +
+            $"\nLocal: {local.Nome}" +
+            $"\nParticipantes: {string.Join(", ", _participantes.Select(p => p.Nome))}" +
             $"\nAnotações: {_anotacoes.Count}";
-    //Falta implementar a ToSring do local, participantes e usuario
   }
 
 }
